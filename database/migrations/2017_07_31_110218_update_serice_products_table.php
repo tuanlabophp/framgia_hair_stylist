51de307c04dcaa307d6b2c9889e1b883d56e8b42
tuanlabophp <tuan.nguyenducanh@gmail.com>
2017-07-31 11:46:49 +0700

    get all service

---
+++ database/migrations/2017_07_31_110218_update_serice_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateSericeProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('service_products', function (Blueprint $table) {
+            $table->float('avg_rate')->nullable()->change();
+            $table->integer('total_rate')->nullable()->change();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('service_products', function (Blueprint $table) {
+            $table->float('avg_rate')->change();
+            $table->integer('total_rate')->change();
+        });
+    }
+}
